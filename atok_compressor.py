ffa482edb9544cc2852963243e19f3e9deef4ba9
Tom Soulanille <soul@prama.com>
2023-12-20 01:55:26 +0000

    atok_compressor: Arithmetic coded token compressor

---
+++ atok_compressor.py
@@ -0,0 +1,309 @@
+"""A tokenizing compressor with arithmetic coding, to assist in validating code structure"""
+
+# The objective is to look like _bz2.BZ2Decompressor
+
+import io
+import logging
+import numpy as np
+import struct
+import tiktoken
+
+from typing import List, Tuple
+
+from ac2_for_z import PDFPredictor, A_to_bin, A_from_bin
+
+
+BUFFER_SIZE = io.DEFAULT_BUFFER_SIZE  # Compressed data read chunk size
+_HEADER = b"\xfe\xfe"                 # Beyond token range
+#_EOS = b"\xff\xff"                    # Beyond token range
+_EOS = b"That's all, folks!"    # DEBUG
+PRECISION = 48                        # The arithmetic coder's arithmetic precision to use
+
+class TokPredictor(PDFPredictor):
+    def __init__(self, max_token, precision=PRECISION):
+        logging.debug(f"TokPredictor({max_token=}, {precision=})")
+        assert 1 < precision < 64 #uint64 limit
+        super().__init__(None, precision)
+        self.max_token = max_token
+
+    # UNNECESSARY I think:
+    def copy(self):
+        return type(self)(self.max_token, self.precision)
+
+
+class ACTokCompressor:
+    """Arithmetic Coding tok compressor"""
+
+    def __init__(self, compression_level=9, encoding_name="gpt2"):
+        # FIXME: This is bz2-specific:
+        if compression_level < 0 or compression_level > 9:
+            raise TypeError
+        self.encoding_name = encoding_name
+        self.tok_enc = tiktoken.get_encoding(encoding_name)
+        self.eot_token = self.tok_enc.encode("<|endoftext|>", allowed_special={"<|endoftext|>"})[0]
+        self.predictor = TokPredictor(self.eot_token)
+        self.predictor.set_cdf_from_pdf([1] * (self.eot_token + 1)) # FIXME: for initial debugging
+        logging.debug(f"ACTokCompressor: {encoding_name} <|endoftext|> is {self.eot_token}")
+        self.a2b = A_to_bin(self.predictor, PRECISION)
+        self.input_accumulator = ""
+        self.bits_accumulator = []
+        self.header_sent = False
+
+    def __repr__(self):
+        return f"comp({self.encoding_name} {' H'[self.header_sent]},{len(self.input_accumulator)}({self.input_accumulator[:4]}) -> {len(self.bits_accumulator)}({''.join(str(v) for v in self.bits_accumulator[:8])})"
+
+    def compress(self, data, *args, **kwargs):
+        # Without reading the entire file before tokenizing, we cannot
+        # trust the tokenization at the margins of each buffer of text
+        # we tokenize.
+        # FIXME: for now we trust that there will be line breaks often
+        # enough, and that a line break is a guarantee of token break.
+        logging.debug(f"ACTokCompressor.compress({len(data)=}) {self=}")
+
+        # Ensure data is in text format
+        if isinstance(data, (bytes, bytearray)):
+            try:
+                data = data.decode('utf-8')  # Decode using UTF-8 or another appropriate encoding
+            except UnicodeDecodeError:
+                raise ValueError("Byte data provided is not in valid UTF-8 format")
+        elif not isinstance(data, str):
+            raise TypeError("Data must be either a string, bytes, or bytearray")
+
+        self.input_accumulator += data
+        if not self.header_sent:
+            logging.debug(f"ACTokCompressor.compress including header in rv")
+            rv = self._header()
+            self.header_sent = True
+        else:
+            rv = b''
+
+        toks = []
+        nl_split = self.input_accumulator.split('\n')
+        for line in nl_split[:-1]:
+            toks.extend(self.tok_enc.encode(line + '\n'))
+        self.input_accumulator = nl_split[-1]
+
+        try:
+            eot_ix = toks.locate(self.eot_token)
+        except:
+            eot_ix = None
+        self.bits_accumulator.extend(list(self.a2b.bits(toks, stop=False)))
+        logging.debug(f"ACTokCompressor.compress {self=}")
+        v, self.bits_accumulator = bits_to_bytes(self.bits_accumulator)
+        rv += v
+        logging.debug(f"ACTokCompressor.compress {self=} {len(rv)=} {rv[:16]}[...]{rv[-16:]} {eot_ix=}")
+        return rv
+
+
+    def flush(self, *args):
+        logging.debug(f"ACTokCompressor.flush({self=}, {args=})")
+        toks = self.tok_enc.encode(self.input_accumulator)
+        toks.append(self.eot_token)
+        try:
+            eot_ix = toks.index(self.eot_token)
+        except ValueError:
+            eot_ix = None
+        logging.debug(f"ACTokCompressor.flush {self=} {toks=} {eot_ix=}")
+        self.bits_accumulator.extend(list(self.a2b.bits(toks, stop=True)))
+        rv, self.bits_accumulator = bits_to_bytes(self.bits_accumulator + [0]*7) # FIXME?
+        logging.debug(f"ACTokCompressor.flush {self=} {rv=} {self.bits_accumulator=}")
+        assert all(v == 0 for v in self.bits_accumulator)
+        self.bits_accumulator = [] # discard padding
+        logging.debug(f"{rv=}")
+        rv += self._footer()
+        logging.debug(f"{rv=}")
+        logging.debug(f"ACTokCompressor.flush {self=} {len(rv)=} {rv[:16]}[...]{rv[-16:]}")
+        return rv
+
+    def _header(self):
+        return _HEADER
+
+    def _footer(self):
+        # FIXME
+        return _EOS
+        #return b"That's all, folks!"
+
+    def __getstate__(self, *args, **kwargs):
+        raise TypeError  # Can't pickle us
+
+
+class ACTokDecompressor:
+    """A tok decompressor"""
+
+    def __init__(self, encoding_name="gpt2"):
+        logging.debug(f"ACTokDecompressor.__init__({encoding_name=})")
+        if not isinstance(encoding_name, (str, bytes)):
+            raise TypeError(f"encoding name is a {type(encoding_name)} want string")
+        self.encoding_name = encoding_name
+        self.tok_enc = tiktoken.get_encoding(encoding_name)
+        self.eot_token = self.tok_enc.encode("<|endoftext|>", allowed_special={"<|endoftext|>"})[0]
+        self.predictor = TokPredictor(self.eot_token)
+        self.predictor.set_cdf_from_pdf([1] * (self.eot_token + 1)) # FIXME: for initial debugging
+        logging.debug(f"ACTokCompressor: {encoding_name} <|endoftext|> is {self.eot_token}")
+        self.dtype = np.dtype('<u2')
+        self._restart()
+
+    def _restart(self):
+        self.n_bytes_ingested = 0
+        self.header_seen = False
+        self.unused_data = b""
+        self.token_buffer = []
+        self.output_buffer = b""
+        self.b2a = A_from_bin(self.predictor, PRECISION)
+        self._eos = False
+
+    def __repr__(self):
+        return f"decomp({' H'[self.header_seen]}{' S'[self._eos]}{' F'[self.eof]},{len(self.unused_data)=}({self.unused_data[:4]},{len(self.token_buffer)=}({self.token_buffer[:4]}...{self.token_buffer[-4:]})) -> {len(self.output_buffer)}({self.output_buffer[:4]}...{self.output_buffer[-4:]}),{self.n_bytes_ingested})"
+        
+    @property
+    def needs_input(self):
+        return len(self.output_buffer) == 0
+
+    @property
+    def eof(self):
+        # return False
+        if self.output_buffer == b"":
+            return self._eos
+        else:
+            return False
+
+
+    def decompress(self, rawblock, size=None, max_length=None):
+        logging.debug(
+            f"ACTokDecompressor.decompress({len(rawblock)=}, {size=}) {self.header_seen=} {len(self.unused_data)=} {self.n_bytes_ingested=}"
+        )
+        logging.debug(f"ACTokDecompressor.decompress {self}")
+
+        # If restarting, clear unused data
+        if self.eof:
+            self._restart()
+
+        self.unused_data += rawblock
+
+        # FIXME: what is size? how is it to be used?
+        if size is not None:
+            if max_length is not None:
+                max_length = min(size, max_length)
+            else:
+                max_length = size
+
+
+        if not self._eos:
+
+            logging.debug(f"ACTokDecompressor.decompress {self}")
+            if not self.header_seen:
+                self.header_seen = self.check_for_header_and_process()
+                if not self.header_seen:
+                    raise OSError("Header expected first")
+            self.n_bytes_ingested += len(rawblock) # FIXME: in the right place here?
+            logging.debug(f"ACTokDecompressor.decompress {self}")
+            new_toks, n_bits = tokens_to_stop_with_bits_consumed(self.b2a, self.unused_data, self.eot_token)
+            try:
+                eot_ix = new_toks.index(self.eot_token)
+            except ValueError:
+                eot_ix = None
+            logging.debug(f"ACTokDecompressor.decompress {self} {eot_ix=}")
+            # HUH? assert n_bits % 8 == 0
+            # FIXME: SUSPECT: dumping unused bits below
+            self.unused_data = self.unused_data[(n_bits+7)//8:] # If we used any bits from that last byte we skip
+            if new_toks[-1] == self.eot_token:
+                logging.debug(f"ACTokDecompressor.decompress hit eot token")
+                assert new_toks.pop() == self.eot_token
+                self._eos = True
+                footer_start, footer_len = self.find_footer()
+                #assert footer_start == 0, f"Expected footer right here, but it's {footer_start} bytes further"
+                if footer_start != 0:
+                    logging.warning(f"Expected footer right here, but it's {footer_start} bytes further")
+                self.unused_data = self.unused_data[footer_start + footer_len:]
+            self.token_buffer.extend(new_toks)
+            logging.debug(f"ACTokDecompressor.decompress {self=}")
+            # Now turn tokens into text in output buffer
+            decoded_toks = self.tok_enc.decode_bytes(self.token_buffer)
+            self.token_buffer = []
+            self.output_buffer += decoded_toks
+            logging.debug(f"ACTokDecompressor.decompress {self=}, {decoded_toks=}")
+
+        # Now provide output, respecting max_length
+        if max_length is None:
+            rv = self.output_buffer
+            self.output_buffer = b""
+        else:
+            rv = self.output_buffer[:max_length]
+            self.output_buffer = self.output_buffer[max_length:]
+            
+        logging.debug(
+            f"ACTokDecompressor.decompress {len(rv)=} {self.header_seen=} {len(self.unused_data)=}"
+        )
+        logging.debug(f"ACTokDecompressor.decompress {self}")
+        return rv
+
+    def check_for_header_and_process(self):
+        if self.unused_data.startswith(_HEADER):
+            self.unused_data = self.unused_data[len(_HEADER) :]
+            return True
+        else:
+            return False
+
+    def find_footer(self):
+        start = self.unused_data.find(_EOS)
+        if start >= 0:
+            rv = start, len(_EOS)
+        else:
+            rv = start, 0
+        logging.debug(f"ACTokDecompressor.find_footer {rv=}")
+        return rv
+
+    def __getstate__(self, *args, **kwargs):
+        raise TypeError
+
+
+# Chat4, tweaked (with my bugfix setting dtype):
+def bits_to_bytes(bitstream: List[int]): #-> Tuple(bytearray, List[int]):
+    # Separate out any tail of the bitstream that doesn't take a full byte
+    # We'll pass it back
+    cut_point = (len(bitstream)//8) * 8
+    bitstream, tail = bitstream[:cut_point], bitstream[cut_point:]
+
+    # Convert the list to a NumPy array
+    bit_array = np.array(bitstream, dtype='uint8')
+
+    if len(bit_array) % 8 != 0:
+        raise ValueError("Length of bitstream should be a multiple of 8")
+
+    # Reshape the array into 8 columns (each row represents a byte)
+    reshaped_array = bit_array.reshape(-1, 8)
+
+    # Convert bits to byte values
+    byte_values = reshaped_array.dot(1 << np.arange(8, dtype='uint8')[::-1])
+
+    # Convert to bytes or bytearray
+    byte_array = bytearray(byte_values)
+
+    return byte_array, tail
+
+# Chat4:
+def bytes_to_bits(byte_data):
+    bitstream = []
+
+    for byte in byte_data:
+        # Convert each byte to a binary string, then to a list of bits
+        bits = [int(bit) for bit in format(byte, '08b')]
+        bitstream.extend(bits)
+
+    return bitstream
+
+# Me:
+def tokens_to_stop_with_bits_consumed(decoder, bs, stop_token=None):
+    masks = [1<<p for p in range(7, -1, -1)]
+    acc = []
+    n = 0
+    for byte in bs:
+        for mask in masks:
+            bit = bool(byte & mask)
+            n += 1
+            vt = decoder(bit)
+            for v in vt:
+                acc.append(v)
+                if v == stop_token:
+                    return acc, n # discarding any tokens after stop
+    return acc, n
---
+++ tests/test_atok_compressor.py
@@ -0,0 +1,172 @@
+"""Test script for atok_compressor"""
+
+#import array
+#import functools
+#import io
+import logging
+#import os
+#import pathlib
+import random
+import struct
+import sys
+
+import pytest
+
+from binascii import hexlify, unhexlify
+from contextlib import contextmanager
+from io import BytesIO, DEFAULT_BUFFER_SIZE
+from typing import Callable, List, Tuple
+
+from unittest.mock import mock_open
+
+import tok_compressor as tc
+import atok_compressor as atc
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)  # StreamHandler logs to console
+
+
+# Now you can use logging in your tests
+def test_example():
+    logging.debug("This is a debug message.")
+
+
+def test_create_file(tmp_path):
+    CONTENT = "content"
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "hello.txt"
+    p.write_text(CONTENT, encoding="utf-8")
+    assert p.read_text(encoding="utf-8") == CONTENT
+    assert len(list(tmp_path.iterdir())) == 1
+
+
+@contextmanager
+def mock_file(initial_data=None):
+    """A context manager for mocking file operations using BytesIO."""
+    # Create a BytesIO object with optional initial data
+    if initial_data is not None:
+        if isinstance(initial_data, str):
+            initial_data = initial_data.encode()  # Convert string to bytes
+        file_obj = BytesIO(initial_data)
+    else:
+        file_obj = BytesIO()
+
+    try:
+        yield file_obj
+    finally:
+        # Perform any necessary cleanup (if any)
+        file_obj.close()
+
+
+def test_mock_file():
+    # Usage example
+    with mock_file("Initial text") as mock_file_obj:
+        # Perform file operations
+        mock_file_obj.write(b" More data")
+        mock_file_obj.seek(0)
+        content = mock_file_obj.read()
+        assert content.decode() == " More dataxt"
+
+
+def test_read_file(mocker):
+    mock_file_contents = "mock file data"
+    mocker.patch("builtins.open", mock_open(read_data=mock_file_contents))
+
+    with open("mock_file.txt", "r") as file:
+        data = file.read()
+
+    assert data == mock_file_contents
+
+
+def test_write_file(mocker):
+    mock_write = mock_open()
+    mocker.patch("builtins.open", mock_write)
+
+    data_to_write = "data to be written"
+    with open("mock_file.txt", "w") as file:
+        file.write(data_to_write)
+
+    mock_write.assert_called_once_with("mock_file.txt", "w")
+    mock_write().write.assert_called_once_with(data_to_write)
+
+def test_bytes_to_bits():
+    y2b = atc.bytes_to_bits
+    assert y2b(b'') == []
+    assert y2b(b'0') == [0,0,1,1, 0,0,0,0]
+    assert y2b(unhexlify('cafe')) == [1,1,0,0, 1,0,1,0, 1,1,1,1, 1,1,1,0]
+
+def test_bits_to_bytes():
+    b2y = atc.bits_to_bytes
+    y2b = atc.bytes_to_bits
+    assert b2y([]) == (b'', [])
+    assert b2y([1]) == (b'', [1])
+    assert b2y([0]*7) == (b'', [0] * 7)
+    assert b2y([1]*7) == (b'', [1] * 7)
+    assert b2y([1]*8) == (b'\xff', [])
+    assert b2y([1]*9) == (b'\xff', [1])
+    for s in ('a5c369', 'deadbeefcafebabe'):
+        assert b2y(y2b(unhexlify(s))) == (unhexlify(s), [])
+    for s in ('foo', 'bar', 'foobar'):
+        b = s.encode('utf8')
+        assert b2y(y2b((b))) == (b, [])
+
+def test_bits_to_bytes_to_bits():
+    b2y = atc.bits_to_bytes
+    y2b = atc.bytes_to_bits
+    random.seed(42)
+    for l in (0, 1, 10, 1024, 1<<20-1):
+        b = random.randbytes(1024)
+        assert b2y(y2b((b))) == (b, [])
+
+
+@pytest.fixture
+def medium_text():
+    return r"""A variety of specific techniques for arithmetic coding have
+historically been covered by US patents, although various well-known
+methods have since passed into the public domain as the patents have
+expired. Techniques covered by patents may be essential for
+implementing the algorithms for arithmetic coding that are specified
+in some formal international standards. When this is the case, such
+patents are generally available for licensing under what is called
+"reasonable and non-discriminatory" (RAND) licensing terms (at least
+as a matter of standards-committee policy). In some well-known
+instances, (including some involving IBM patents that have since
+expired), such licenses were available for free, and in other
+instances, licensing fees have been required. The availability of
+licenses under RAND terms does not necessarily satisfy everyone who
+might want to use the technology, as what may seem "reasonable" for a
+company preparing a proprietary commercial software product may seem
+much less reasonable for a free software or open source project."""
+
+def compress_decompress_test(text):
+    c = atc.ACTokCompressor()
+    d = atc.ACTokDecompressor()
+    compressed = c.compress(text) + c.flush()
+    decompressed = d.decompress(compressed)
+    if type(text) is str: text = text.encode("utf8")
+    assert text == decompressed
+    
+def test_compress_short():
+    compress_decompress_test(b"")
+    compress_decompress_test(b"Hi!")
+    compress_decompress_test(b"Hi!Hi!")
+    compress_decompress_test(b"The quick brown fox, et al.")
+
+def test_compress_short_nl():
+    for c,d in ((tc.TokCompressor(),tc.TokDecompressor()), (atc.ACTokCompressor(),atc.ACTokDecompressor())):
+        text = b"\n"
+        zbody = c.compress(text)
+        ztail = c.flush()
+        reconstructed = d.decompress(zbody + ztail)
+        assert text == reconstructed
+
+def test_compress_brief():
+    compress_decompress_test(b"The quick brown fox jumped over the lazy dogs.\n")
+
+def test_compress_medium(medium_text):
+    compress_decompress_test(medium_text)
